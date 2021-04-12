94e9860e82bc704e35d0c7a379da6886c6009e3f
VictorRamirez18 <vramirez100443@ppsc.edu.mx>
2021-04-12 18:15:08 -0500

    Cartas responsivas

--- src/App.js
+++ src/App.js
@@ -19,10 +19,9 @@ class App extends React.Component {
       });
   }
   handleChange = (event) => {
-    let { value } = event.target;
+    let { name, value } = event.target;
     value = value.toLowerCase();
-    this.setState({ busqueda: value });
-    console.log(this.state.busqueda);
+    this.setState({ [name]: value });
   };
   render() {
     return (
@@ -31,12 +30,9 @@ class App extends React.Component {
           busqueda={this.state.busqueda}
           onChange={this.handleChange}
         ></HeaderBar>
-        {console.log(this.state.isFetch)}
         {this.state.isFetch && (
           <div className="contenedor mt-5 container h-auto m-auto p-5 flex flex-row flex-wrap justify-center">
-            {console.log(this.state.pokemon)}
             {this.state.pokemon.map((item, index) => {
-              // console.log(item);
               return (
                 <PokemonCards
                   pokemones={item}
@@ -69,9 +65,11 @@ function HeaderBar(props) {
         <label>
           <input
             type="text"
+            name="busqueda"
             value={props.busqueda}
             onChange={props.onChange}
-            className="mr-2"
+            className="mr-2 p-1 lg:w-48 sm:w-1/4"
+            placeholder="busqueda"
           ></input>
           Search
         </label>
@@ -89,7 +87,6 @@ class PokemonCards extends React.Component {
   }
 
   componentDidMount() {
-    console.log(this.props.pokemones);
     fetch(this.props.pokemones.url).then((response) =>
       response.json().then((response) => {
         this.setState({ pokemons2: response });
@@ -99,21 +96,21 @@ class PokemonCards extends React.Component {
   render() {
     const name = this.props.pokemones.name;
     const busqueda = this.props.busqueda;
-    if (this.state.pokemons2.sprites === undefined) return "Cargando";
+    if (this.state.pokemons2.sprites === undefined) return <p>"Cargando"</p>;
     else {
       if (name.startsWith(busqueda) || busqueda === "") {
         return (
-          <div className="card bg-white my-2.5 mx-2.5 p-2 flex flex-col justify-around">
+          <div className="card bg-white my-2.5 mx-2.5 p-2 flex flex-col justify-around lg:w-1/6 sm:w-1/4 h-64">
             <p className="text-2xl text-center">{this.props.pokemones.name}</p>
             <img
-              src={this.state.pokemons2.sprites.front_shiny}
+              src={this.state.pokemons2.sprites.front_default}
               className="h-52 object-cover my-1.5"
             ></img>
             <div className="flex flex-row flex-wrap justify-around my-1.5">
               {this.state.pokemons2.types.map((item, index) => {
                 return (
                   <div
-                    className="w-auto p-2 rounded-lg border-2 border-black"
+                    className="w-auto p-2 rounded-lg border-2 border-black capitalize"
                     id={item.type.name}
                     key={index}
                   >
